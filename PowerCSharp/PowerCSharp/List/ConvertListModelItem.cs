9b475e2d94bf3d842209cbcf8f06846fddc1f8a2
TUTORABC\bradchen <bradchen@tutorabc.com>
2017-11-03 23:08:05 +0800

    add file

---
+++ PowerCSharp/PowerCSharp/List/ConvertListModelItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerCSharp.List
+{
+    class ConvertListModelItem
+    {
+        public void test()
+        {
+            var myCars = new Car().getList();
+
+            List<Car2> all = myCars.ConvertAll(x => x.ConvertTo<Car2>()); 
+        }
+    }
+
+    public class Car2
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
